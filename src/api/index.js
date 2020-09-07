5f1eec4c5f9d7585ddec5447723fd23d606c3693
rickychhoukdean <chhoukdean184@msn.com>
2020-09-07 16:18:27 -0400

    fix bug

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 export const fetchList = async (search, page) => {
   const response = await fetch(
-    `http://www.omdbapi.com/?s=${search}&page=${page}&type=movie&apikey=3a117c14`
+    `https://www.omdbapi.com/?s=${search}&page=${page}&type=movie&apikey=3a117c14`
   );
   let movies = await response.json();
   return movies.Response === "True" ? movies.Search : [];
